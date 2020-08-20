1914ca35b12d4dec201d477fd9073e98de17e751
Heinrich <Heinrich>
2020-08-20 11:32:35 +0200

    Integrater can be set by rviz pose estimation.

--- mir_hardware_helper/include/mir_hardware_helper/vel_integrator.hpp
+++ mir_hardware_helper/include/mir_hardware_helper/vel_integrator.hpp
@@ -1,6 +1,8 @@
 #include<ros/ros.h>
 #include<geometry_msgs/TwistStamped.h>
 #include<mir_hardware_helper/SetInitialPose.h>
+#include<geometry_msgs/PoseWithCovarianceStamped.h>
+#include<std_srvs/Empty.h>
 #include<tf/tf.h>
 
 
@@ -9,12 +11,21 @@ class VelIntegrator{
     VelIntegrator(ros::NodeHandle &nh,std::string topic_name_vel,std::string topic_name_pose):nh_(nh)
     {
         this->sub_=this->nh_.subscribe(topic_name_vel,10,&VelIntegrator::callbackIntegrate,this);
+        this->pose_initializer_=this->nh_.subscribe("initialpose",10,&VelIntegrator::callbackSetInitial,this);
         this->pub_=this->nh_.advertise<geometry_msgs::PoseStamped>(topic_name_pose,10);
-        this->initial_server_=this->nh_.advertiseService("set_initial_pose",&VelIntegrator::callbackSetInitial,this);
+        ros::NodeHandle priv("~");
+        this->initial_server_=priv.advertiseService("reset",&VelIntegrator::callbackReset,this);
         this->pose_.setIdentity();
+        
+        priv.getParam("topic_name",this->pose_topic_);
+        priv.getParam("frame_id",this->frame_id_);
     }
 
     private:
+    void callbackSetInitial(geometry_msgs::PoseWithCovarianceStamped msg)
+    {
+        tf::poseMsgToTF(msg.pose.pose,this->pose_);
+    }
     void callbackIntegrate(geometry_msgs::TwistStamped msg)
     {
         ros::Duration d_t;
@@ -39,19 +50,24 @@ class VelIntegrator{
         geometry_msgs::PoseStamped pose_msg;
         tf::poseTFToMsg(this->pose_,pose_msg.pose);
         pose_msg.header.stamp=now;
+        pose_msg.header.frame_id=this->frame_id_;
         this->pub_.publish(pose_msg);         
     }
-    bool callbackSetInitial(mir_hardware_helper::SetInitialPoseRequest &req,mir_hardware_helper::SetInitialPoseResponse &res)
+    bool callbackReset(std_srvs::EmptyRequest &req,std_srvs::EmptyResponse  &res)
     {
-        tf::poseMsgToTF(req.pose.pose,this->pose_);
-        res.succeed=true;
-        this->time_=ros::Time::now();
+        tf::poseMsgToTF(ros::topic::waitForMessage<geometry_msgs::PoseStamped>(this->pose_topic_,this->nh_)->pose,this->pose_);
+        return true;
     }
-    tf::Pose pose_;
+    
+    std::string frame_id_;
+    tf::Pose pose_;    
     ros::Time time_;
+    std::string pose_topic_;
     ros::NodeHandle nh_;
     ros::Subscriber sub_;
+    ros::Subscriber pose_initializer_;
     ros::Publisher pub_;
+    ros::ServiceServer initial_pose_server_;
     ros::ServiceServer initial_server_;
 
 };

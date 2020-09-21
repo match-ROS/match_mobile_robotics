110024a51d3db5fecdb8f54884d8993bac8f040e
Heinrich <Heinrich>
2020-09-21 09:37:06 +0200

    Fixed startup bug (prot stop due running prog)

--- ur_hardware_helper/ur_launcher/scripts/enable_ur.py
+++ ur_hardware_helper/ur_launcher/scripts/enable_ur.py
@@ -3,6 +3,7 @@
 import rospy
 import actionlib
 from ur_dashboard_msgs.msg import SetModeAction,SetModeGoal
+from std_srvs.srv import Trigger,TriggerRequest
 
 def feedbackCallback(feedback):
     rospy.loginfo("Feedback:")
@@ -11,6 +12,7 @@ def feedbackCallback(feedback):
 def activeCallback():
     rospy.loginfo("Enabling ur!")
 
+
 def doneCallback(state,result):
     if result.success: 
         rospy.loginfo("Done:")
@@ -18,22 +20,30 @@ def doneCallback(state,result):
         rospy.loginfo(state)
         rospy.loginfo("Result:")
         rospy.loginfo(result)
-        rospy.signal_shutdown("Enabeling done")
     else:
         rospy.logwarn("Done:")
         rospy.logwarn("State:")
         rospy.logwarn(state)
         rospy.logwarn("Result:")
         rospy.logwarn(result)
-        rospy.signal_shutdown("Enabeling done")
 
 
 if __name__=="__main__":
     rospy.init_node("ur_enabler")
     client=actionlib.SimpleActionClient("ur_hardware_interface/set_mode",SetModeAction)
     client.wait_for_server()
-    goal=SetModeGoal()
-    goal.target_robot_mode=7
-    goal.play_program=True
-    client.send_goal(goal,feedback_cb=feedbackCallback,done_cb=doneCallback,active_cb=activeCallback)
-    rospy.spin()
+    start_client=rospy.ServiceProxy("ur_hardware_interface/dashboard/play",Trigger)
+    stop_client=rospy.ServiceProxy("ur_hardware_interface/dashboard/stop",Trigger)
+    
+    trigger=TriggerRequest()
+    
+    stop_client.call(trigger)
+    rospy.sleep(2)
+    goal_start=SetModeGoal()
+    goal_start.target_robot_mode=7
+    client.send_goal(goal_start,feedback_cb=feedbackCallback,done_cb=doneCallback,active_cb=activeCallback)
+    client.wait_for_result()
+
+    rospy.sleep(2)
+    start_client.call(trigger)   
+    

288a27cc4d7e76b92e57afc08ad1ca5cf89a8556
hlurz <henrik.lurz@web.de>
2022-02-08 10:28:33 +0100

    Changed ip to new miranda ip Changed hour offset to winter time

--- general_hardware_helper/master_time_sync/master_time_sync.py
+++ general_hardware_helper/master_time_sync/master_time_sync.py
@@ -4,8 +4,8 @@ import os
 from datetime import datetime, timedelta
 import requests, json
 
-ip = "192.168.12.20" # This IP is for executing on the robot
-# ip = "10.110.130.23" # This IP is for executing on the master computer
+# ip = "192.168.12.20" # This IP is for executing on the robot
+ip = "10.110.130.103" # This IP is for executing on the master computer
 host = 'http://' + ip + '/api/v2.0.0/'
 
 headers = {}
@@ -17,7 +17,7 @@ status_request = requests.get(host + 'status', headers = headers)
 header = status_request.headers
 robot_time: str = header["Date"]
 time: datetime = datetime.strptime(robot_time, "%a, %d %b %Y %H:%M:%S %Z")
-new_time = time + timedelta(hours=2)
+new_time = time + timedelta(hours=1)
 
 print("Current time of MiR in GMT: " + str(time))
 print("Current time of MiR in MESZ: " + str(new_time))

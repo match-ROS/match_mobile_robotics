afa1b8f5d69f421913eee6ad1d67eff79d85a009
matchRos <47657974+matchRos@users.noreply.github.com>
2022-03-28 17:47:10 +0200

    Add files via upload

--- ur/ur_controllers_match/scripts/ur_command_smoother.py
+++ ur/ur_controllers_match/scripts/ur_command_smoother.py
@@ -14,7 +14,7 @@ class ur_command_smoother():
         self.actual_twist = Twist()
         twist_command = Twist()
         self.target_twist = Twist()
-        #self.twist_old = Twist()
+        self.last_msg_timestamp = rospy.get_time()
         
         rospy.Subscriber("/ur/unsmooth_twist",Twist,self.unsmooth_twist_cb)
         self.smooth_twitst_pub = rospy.Publisher("/twist_controller/command",Twist,queue_size=1)
@@ -48,12 +48,12 @@ class ur_command_smoother():
                 
                 
             # compute target acceleration
-            acc_lin_x = twist_command.linear.x - self.actual_twist.linear.x
-            acc_lin_y = self.target_twist.linear.y - twist_command.linear.y
-            acc_lin_z = self.target_twist.linear.z - twist_command.linear.z
-            acc_ang_x = self.target_twist.angular.x - twist_command.angular.x
-            acc_ang_y = self.target_twist.angular.y - twist_command.angular.y
-            acc_ang_z = self.target_twist.angular.z - twist_command.angular.z
+            acc_lin_x = twist_command.linear.x  - self.actual_twist.linear.x
+            acc_lin_y = twist_command.linear.y  - self.actual_twist.linear.y
+            acc_lin_z = twist_command.linear.z  - self.actual_twist.linear.z
+            acc_ang_x = twist_command.angular.x - self.actual_twist.angular.x
+            acc_ang_y = twist_command.angular.y - self.actual_twist.angular.y
+            acc_ang_z = twist_command.angular.z - self.actual_twist.angular.z
             
             # limit target acceleration
             if abs(acc_lin_x) > self.max_acc_lin_x:
@@ -81,20 +81,27 @@ class ur_command_smoother():
             else:   
                  self.actual_twist.angular.y += acc_ang_y
                  
-            if abs(acc_ang_y) > self.max_acc_ang_y:
-                        self.actual_twist.angular.x += acc_ang_y / abs(acc_ang_y) * self.max_acc_ang_y
+            if abs(acc_ang_z) > self.max_acc_ang_z:
+                        self.actual_twist.angular.z += acc_ang_z / abs(acc_ang_z) * self.max_acc_ang_z
             else:   
-                 self.actual_twist.angular.y += acc_ang_y
+                 self.actual_twist.angular.z += acc_ang_z
                  
- 
+
+            self.smooth_twitst_pub.publish(self.actual_twist)
+            
+            
+            # check for timestamp
+            if abs(self.last_msg_timestamp-rospy.get_time()) >  self.timeout:
+                self.target_twist = Twist()   
                  
-                 
-            rospy.loginfo_throttle(0.2,[self.actual_twist.linear.x,self.max_vel_lin_x])
+            rospy.loginfo_throttle(0.2,self.actual_twist)
             Rate.sleep()
                 
         
     def config(self):
         self.rate = rospy.get_param('~publish_rate',100)
+        self.timeout = rospy.get_param('~timeout',0.1)
+        
         self.max_vel_lin_x = rospy.get_param('~max_vel_lin_x',0.0)
         self.max_vel_lin_y = rospy.get_param("~max_vel_lin_y",0.0)
         self.max_vel_lin_z = rospy.get_param("~max_vel_lin_z",0.0)
@@ -111,7 +118,7 @@ class ur_command_smoother():
         
         
     def unsmooth_twist_cb(self,Twist):
-        twist_command = Twist
+        self.last_msg_timestamp = rospy.get_time()
         self.target_twist = Twist
         print("callback")
         

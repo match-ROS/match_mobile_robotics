8c0f212c7a20ae80d2d8b7c111e8bb8bc13337c9
matchRos <Recker@match.uni-hannover.de>
2022-07-25 15:34:09 +0200

    ps4 controller refactoring

--- general_hardware_helper/ps4_controller/scripts/PlaystationHandler.py
+++ general_hardware_helper/ps4_controller/scripts/PlaystationHandler.py
@@ -4,8 +4,8 @@ from sensor_msgs.msg import Joy
 class PlayStationHandler():
     X=0
     CIRC=1
-    RECT=2
-    TRI=3
+    TRI=2
+    RECT=3
     OPTIONS=7
     SHARE=6
     PS=8
@@ -25,17 +25,27 @@ class PlayStationHandler():
                 AX1:"AX1",
                 AX2:"AX2"}
 
+# AXES IDX:
+    L2=2
+    R2=5
+    LS_H=0
+    LS_V=1
+    RS_H=3
+    RS_V=4
+    DPAD_H=6
+    DPAD_V=7
+
     def __init__(self):
         self.__sub_joy = rospy.Subscriber('joy', Joy, self.__joy_callback__)
-        self._buttons=[0]*11
-        self._edges=[0]*11
-        self._axes=[0]*8
+        self._buttons=[0] * 11
+        self._edges=[0] * 11
+        self._axes=[0] * 8
         
     
-    def __joy_callback__(self,msg):
-        self._edges=[a and not b for a,b in zip(msg.buttons ,self._buttons)]        
-        self._buttons=msg.buttons
-        self._axes=msg.axes
+    def __joy_callback__(self, msg):
+        self._edges = [a and not b for a, b in zip(msg.buttons, self._buttons)]        
+        self._buttons = msg.buttons
+        self._axes = msg.axes
 
 
     def run(self):
--- general_hardware_helper/ps4_controller/src/ps4_controller/__init__.py
+++
@@ -1,1 +0,0 @@
-from PlaystationHandler import PlayStationHandler

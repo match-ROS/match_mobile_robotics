966e241b610d7a96a68dec50dd218d709dc606b0
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-12-15 13:21:27 +0100

    added service calls and check if picked

--- ur/ur_controllers_match/scripts/schunk_controller.py
+++ ur/ur_controllers_match/scripts/schunk_controller.py
@@ -2,11 +2,14 @@
 
 import rospy
 from ur_msgs.srv import SetIO
+from ur_msgs.msg import IOStates
+from std_srvs.srv import Trigger, TriggerResponse
+from std_msgs.msg import Bool, Int8
 
 from typing import Optional, List
 
 class SchunkController():
-    def __init__(self, prefix_ur="ur/", pin_magnet=0, pin_demagnet=1, pin_force_a=2, pin_force_b=3):
+    def __init__(self, prefix_ur="ur/", pin_magnet=0, pin_demagnet=1, pin_picked=7, pin_force_a=2, pin_force_b=3):
         """To control the Schunk gripper
 
         Args:
@@ -26,6 +29,7 @@ class SchunkController():
         self.pin_demagnet=pin_demagnet
         self.pin_force_a=pin_force_a
         self.pin_force_b=pin_force_b
+        self.pin_picked=pin_picked
         
         rospy.init_node('schunk_controller_node', anonymous=True)
         self.config()
@@ -37,6 +41,55 @@ class SchunkController():
         self.srv(1 ,self.pin_demagnet, False) # fun, pin, state (fun=1: digital out)
         self.srv(1 ,self.pin_force_a, False) # fun, pin, state (fun=1: digital out)
         self.srv(1 ,self.pin_force_b, False) # fun, pin, state (fun=1: digital out)
+        
+        rospy.Service(self.prefix_ur+'schunk/grasp', Trigger, self.cb_grasp_srv)
+        rospy.Service(self.prefix_ur+'schunk/release', Trigger, self.cb_release_srv)
+        rospy.Service(self.prefix_ur+'schunk/is_picked', Trigger, self.cb_is_picked_srv)
+        
+        
+    def cb_grasp_srv(self, req: Trigger):
+        self.grasp()
+        if self.is_picked():
+            return TriggerResponse(True, "Grasp successfull")
+        return TriggerResponse(False, "Grasp NOT successfull")
+    
+    def cb_release_srv(self, req: Trigger):
+        self.release()
+        if not self.is_picked():
+            return TriggerResponse(True, "Release successfull")
+        return TriggerResponse(False, "Release NOT successfull")
+    
+    def cb_is_picked_srv(self, req: Trigger):
+        if self.is_picked():
+            return TriggerResponse(True, "Object is picked")
+        return TriggerResponse(False, "Object is NOT picked")
+        
+    def listen_to_topics(self):
+        """Listen to topics to grasp and release the object
+        
+        Topics:
+            prefix_ur+"release" takes a bool
+            prefix_ur+"grasp" takes an int from 0 to 3
+        """
+        rospy.Subscriber(self.prefix_ur+"release", Bool, self.cb_release)
+        rospy.Subscriber(self.prefix_ur+"grasp", Int8, self.cb_grasp)
+        
+    def is_picked(self):
+        try:
+            msg=rospy.wait_for_message(self.prefix_ur+"ur_hardware_interface/io_states", IOStates, timeout=10)
+            return msg.digital_in_states[self.pin_picked].state 
+        except rospy.ROSException:
+            rospy.logerr("No message from IOStates")
+            return False
+    
+    def cb_release(self, msg):
+        if msg.data:
+            self.release()
+        else:
+            self.grasp()
+    
+    def cb_grasp(self, msg):
+        self.grasp(msg.data)
         
     def test(self):
         rate = rospy.Rate(1)
@@ -60,10 +113,14 @@ class SchunkController():
             bool: success
         """
         if not 0 <= force <= 3 or not isinstance(force, int):
-            raise ValueError("force must be 0, 1, 2, or 3")
-        force = bin(force)
-        force_a = bool(int(force[-2]))
-        force_b = bool(int(force[-1]))
+            # raise ValueError("force must be 0, 1, 2, or 3")
+            rospy.logerr("force must be 0, 1, 2, or 3")
+            return False
+        # force = bin(force)
+        # force_a = bool(int(force[-2]))
+        # force_b = bool(int(force[-1]))
+        force_a = int(force/2)
+        force_b = int(force%2)
         self.set_pins([self.pin_force_a, self.pin_force_b, self.pin_demagnet], [force_a, force_b, False])
         rospy.sleep(0.001) # wait for the signal to be set
         self.srv(1, self.pin_magnet, True)
@@ -95,6 +152,8 @@ class SchunkController():
             self.srv(fun, pin, state)
         
 if __name__ == '__main__':
+    #DI 7 ist input pick
     gripper=SchunkController(prefix_ur="mur/ur/")
-    gripper.test()
+    rospy.spin()
+    # gripper.test()
     

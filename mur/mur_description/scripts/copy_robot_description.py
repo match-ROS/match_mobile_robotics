9cd8a07c0d6cb5f731e739732650247e470fe0c4
Tobias Recker <Recker@match.uni-hannover.de>
2022-12-31 00:27:19 +0100

    resolved model_name / ns issues

--- mur/mur_description/scripts/copy_robot_description.py
+++ mur/mur_description/scripts/copy_robot_description.py
@@ -5,9 +5,10 @@ import rospy
 
 def main():
     rospy.init_node("copy_robot_description_node", anonymous=True)
-    mur_ns = rospy.get_param("~mur_ns", "mur620")
-    ur_prefix = rospy.get_param("~ur_prefix", "UR10")
-    robot_description = rospy.get_param("/mur620/robot_description")
+    rospy.sleep(1.0)
+    mur_ns = rospy.get_param('~mur_ns')
+    ur_prefix = rospy.get_param('~ur_prefix', "UR10")
+    robot_description = rospy.get_param("/" + mur_ns + "/robot_description")
     rospy.set_param("/" + mur_ns + "/" + ur_prefix + "_l/robot_description", robot_description)
     rospy.set_param("/" + mur_ns + "/" + ur_prefix + "_r/robot_description", robot_description)
 

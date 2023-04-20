97e902bff63d0fcd1e262cca998297838e1c8937
YassineMch <yassine.mechri@stu.uni-hannover.de>
2023-04-20 17:47:21 +0200

    added robot pose option

--- mir/mir_navigation/nodes/mir_pose_simple.py
+++ mir/mir_navigation/nodes/mir_pose_simple.py
@@ -14,6 +14,7 @@ class Mir_pose_simple():
         self.amcl_pose_topic = rospy.get_param("~amcl_pose_topic", "amcl_pose")
         self.groud_truth_topic = rospy.get_param("~groud_truth_topic", "groud_truth")
         self.mocap_topic = rospy.get_param("~mocap_topic", "/qualisys/mur620b/pose")
+        self.robot_pose_topic = rospy.get_param("~robot_pose_topic", "robot_pose")
         
         # Subscribe to the localization topic based on the localization_type parameter
         if self.localization_type == "amcl":
@@ -24,6 +25,8 @@ class Mir_pose_simple():
             rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback)
         elif self.localization_type == "mocap":
             rospy.Subscriber(self.mocap_topic, PoseStamped, self.mocap_callback)
+        elif self.localization_type == "robot_pose":
+            rospy.Subscriber(self.robot_pose_topic, Pose, self.robot_pose_callback)
 
         # Initialize the publishers
         self.pose_pub = rospy.Publisher('mir_pose_simple', Pose, queue_size=1)
@@ -44,6 +47,14 @@ class Mir_pose_simple():
     def mocap_callback(self, msg):
         self.pose_pub.publish(msg.pose)
         self.pose_stamped_pub.publish(msg)
+        
+    def robot_pose_callback(self, msg):
+        self.pose_pub.publish(msg)
+        pose_msg = PoseStamped()
+        pose_msg.header.frame_id = "map"
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.pose = msg
+        self.pose_stamped_pub.publish(pose_msg)
 
 if __name__ == '__main__':
     try:

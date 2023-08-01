67d98971fe184f61863065ac215b91793f279786
Hauke Heeren <heeren@match.uni-hannover.de>
2023-08-01 17:34:26 +0200

    added mir_srvs

---
+++ mir/mir_srvs/cmake/mir_srvs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${mir_srvs_DIR}/.." "" mir_srvs_MSG_INCLUDE_DIRS UNIQUE)
+set(mir_srvs_MSG_DEPENDENCIES std_msgs;geometry_msgs;rosgraph_msgs;sensor_msgs;mirMsgs)

5f6673cb4bad18286eb765fa9e1974b50e2b07b5
Tobias Recker <Recker@match.uni-hannover.de>
2024-02-05 13:44:00 +0100

    added global tcp_pose publisher

--- mur/mur_control/scripts/global_tcp_pose_publisher.py
+++ mur/mur_control/scripts/global_tcp_pose_publisher.py
@@ -13,6 +13,8 @@ class GlobalTCPPosePublisher():
         self.base_frame = rospy.get_param('base_frame', 'map')
         self.local_TCP_pose_topic = rospy.get_param('local_TCP_pose_topic', '/mur620a/UR10_l/tcp_pose')
 
+        self.local_TCP_pose = Pose()
+
         rospy.Subscriber(self.local_TCP_pose_topic, Pose, self.local_TCP_pose_callback)
 
     def tcp_pose_publisher(self):
@@ -24,18 +26,26 @@ class GlobalTCPPosePublisher():
         
         rate = rospy.Rate(self.rate)  # 10 Hz
         
-        tf_listener.waitForTransform(self.base_frame, self.UR_base_link_name, rospy.Time(), rospy.Duration(4.0))
+        tf_listener.waitForTransform(self.base_frame, self.UR_base_link_name, rospy.Time(), rospy.Duration(8.0))
 
         while not rospy.is_shutdown():
             try:
+                # get transform between map and UR base link
                 trans, rot = tf_listener.lookupTransform(self.base_frame, self.UR_base_link_name, rospy.Time(0))
+
+                # transform local TCP pose to map frame and add to the UR base link pose
                 pose = PoseStamped()
                 pose.header.stamp = rospy.Time.now()
                 pose.header.frame_id = self.base_frame
-                pose.pose.position.x =  trans[0]
-                pose.pose.position.y =  trans[1]
-                pose.pose.position.z =  trans[2]
-                q = quaternion_from_euler(rot[0], rot[1], rot[2])
+
+                R = tf.transformations.quaternion_matrix([rot[0], rot[1], rot[2], rot[3]])
+                pose.pose.position.x = trans[0] + R[0,0]*self.local_TCP_pose.position.x + R[0,1]*self.local_TCP_pose.position.y + R[0,2]*self.local_TCP_pose.position.z
+                pose.pose.position.y = trans[1] + R[1,0]*self.local_TCP_pose.position.x + R[1,1]*self.local_TCP_pose.position.y + R[1,2]*self.local_TCP_pose.position.z
+                pose.pose.position.z = trans[2] + R[2,0]*self.local_TCP_pose.position.x + R[2,1]*self.local_TCP_pose.position.y + R[2,2]*self.local_TCP_pose.position.z
+
+                R_local = tf.transformations.quaternion_matrix([self.local_TCP_pose.orientation.x, self.local_TCP_pose.orientation.y, self.local_TCP_pose.orientation.z, self.local_TCP_pose.orientation.w])
+                R_combined = tf.transformations.concatenate_matrices(R, R_local)
+                q = tf.transformations.quaternion_from_matrix(R_combined)
                 pose.pose.orientation.x = q[0]
                 pose.pose.orientation.y = q[1]
                 pose.pose.orientation.z = q[2]
@@ -44,7 +54,10 @@ class GlobalTCPPosePublisher():
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
 
-    
+            rate.sleep()
+
+    def local_TCP_pose_callback(self, data):
+        self.local_TCP_pose = data
 
                 
 

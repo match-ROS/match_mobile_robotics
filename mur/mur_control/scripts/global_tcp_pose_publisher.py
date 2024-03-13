9c831329c7e0ed0568de1c32517d273e130c8a9e
Tobias Recker <Recker@match.uni-hannover.de>
2024-03-13 17:24:10 +0100

    added global tcp pose publisher for right ur

--- mur/mur_control/scripts/global_tcp_pose_publisher.py
+++ mur/mur_control/scripts/global_tcp_pose_publisher.py
@@ -9,9 +9,9 @@ class GlobalTCPPosePublisher():
 
     def __init__(self):
         self.rate = rospy.get_param('rate', 100.0)
-        self.UR_base_link_name = rospy.get_param('UR_base_link_name', 'mur620a/UR10_l/base_link_inertia')
-        self.base_frame = rospy.get_param('base_frame', 'map')
-        self.local_TCP_pose_topic = rospy.get_param('local_TCP_pose_topic', '/mur620a/UR10_l/ur_calibrated_pose')
+        self.UR_base_link_name = rospy.get_param('~UR_base_link_name', 'mur620a/UR10_l/base_link_inertia')
+        self.base_frame = rospy.get_param('~base_frame', 'map')
+        self.local_TCP_pose_topic = rospy.get_param('~local_TCP_pose_topic', '/mur620a/UR10_l/ur_calibrated_pose')
 
         self.local_TCP_pose = Pose()
 

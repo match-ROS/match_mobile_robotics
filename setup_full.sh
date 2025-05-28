63b2b7ecc337f50fac06f789c460a4c4e8590c0c
Tobias Recker <Recker@match.uni-hannover.de>
2025-05-28 11:51:07 +0200

    able to build

--- setup_full.sh
+++ setup_full.sh
@@ -16,19 +16,20 @@ catkin build
 source devel/setup.bash
 
 # install dependencies manually (this should usually be done through rosdep) 
-sudo apt install ros-noetic-costmap-2d
-sudo apt install ros-noetic-serial
-sudo apt install ros-noetic-nav-core
-sudo apt install ros-noetic-moveit-core
-sudo apt install ros-noetic-ur-client-library
-sudo apt install ros-noetic-moveit-ros-planning-interface
-sudo apt install ros-noetic-mbf-msgs
-sudo apt install ros-noetic-mir-actions
-sudo apt install ros-noetic-navfn
-sudo apt install ros-noetic-industrial-robot-status-interface
-sudo apt install ros-noetic-move-base-msgs
-sudo apt install ros-noetic-scaled-joint-trajectory-controller
-sudo apt install ros-noetic-rospy-message-converter
-sudo apt install ros-noetic-speed-scaling-interface
-sudo apt install ros-noetic-speed-scaling-state-controller
-sudo apt install ros-noetic-pass-through-controllers
+sudo apt install ros-one-costmap-2d
+sudo apt install ros-one-serial
+sudo apt install ros-one-nav-core
+sudo apt install ros-one-moveit-core
+sudo apt install ros-one-ur-client-library
+sudo apt install ros-one-moveit-ros-planning-interface
+sudo apt install ros-one-mbf-msgs
+sudo apt install ros-one-mir-actions
+sudo apt install ros-one-navfn
+sudo apt install ros-one-industrial-robot-status-interface
+sudo apt install ros-one-move-base-msgs
+sudo apt install ros-one-scaled-joint-trajectory-controller
+sudo apt install ros-one-rospy-message-converter
+sudo apt install ros-one-speed-scaling-interface
+sudo apt install ros-one-speed-scaling-state-controller
+sudo apt install ros-one-pass-through-controllers
+sudo apt install libserial-dev

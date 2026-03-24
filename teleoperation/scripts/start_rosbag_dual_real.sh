14377c9fc9a07a6db84f79a2227dddae23dc2dd6
Simone Pantanetti <simone.pantanetti@gmail.com>
2026-03-24 15:34:24 +0100

    Commentate alcune righe sullo script

--- teleoperation/scripts/start_rosbag_dual_real.sh
+++ teleoperation/scripts/start_rosbag_dual_real.sh
@@ -33,62 +33,62 @@ TOPICS=(
   /mur620b/UR10_r/twist_controller/command_collision_free
   /mur620d/UR10_l/twist_controller/command_collision_free
   /mur620d/UR10_r/twist_controller/command_collision_free
-  /mur620b/UR10_l/twist_controller/command_collision_free_stamped
-  /mur620b/UR10_r/twist_controller/command_collision_free_stamped
-  /mur620b/UR10_l/twist_controller/controller_input
-  /mur620b/UR10_r/twist_controller/controller_input
-  /mur620d/UR10_l/twist_controller/controller_input
-  /mur620d/UR10_r/twist_controller/controller_input
+  #/mur620b/UR10_l/twist_controller/command_collision_free_stamped
+  #/mur620b/UR10_r/twist_controller/command_collision_free_stamped
+  #/mur620b/UR10_l/twist_controller/controller_input
+  #/mur620b/UR10_r/twist_controller/controller_input
+  #/mur620d/UR10_l/twist_controller/controller_input
+  #/mur620d/UR10_r/twist_controller/controller_input
   /mur620b/UR10_l/global_tcp_pose
   /mur620b/UR10_r/global_tcp_pose
   /mur620d/UR10_l/global_tcp_pose
   /mur620d/UR10_r/global_tcp_pose
-  /mur620b/joint_states
-  /mur620d/joint_states
+  #/mur620b/joint_states
+  #/mur620d/joint_states
   /mur620b/UR10_l/joint_states
   /mur620b/UR10_r/joint_states
   /mur620d/UR10_l/joint_states
   /mur620d/UR10_r/joint_states
-  /mur620b/UR10_l/ewellix_tlt_node_l/joint_states_lift
-  /mur620b/UR10_r/ewellix_tlt_node_r/joint_states_lift
-  /mur620d/UR10_l/ewellix_tlt_node_l/joint_states_lift
-  /mur620d/UR10_r/ewellix_tlt_node_r/joint_states_lift
+  #/mur620b/UR10_l/ewellix_tlt_node_l/joint_states_lift
+  #/mur620b/UR10_r/ewellix_tlt_node_r/joint_states_lift
+  #/mur620d/UR10_l/ewellix_tlt_node_l/joint_states_lift
+  #/mur620d/UR10_r/ewellix_tlt_node_r/joint_states_lift
   /mur620b/mir_pose_stamped_simple
   /mur620d/mir_pose_stamped_simple
-  /qualisys_map/mur620b/pose
-  /qualisys_map/mur620d/pose
-  /mur620b/UR10_r/global_tcp_pose_mocap
-  /mur620d/UR10_r/global_tcp_pose_mocap
+  #/qualisys_map/mur620b/pose
+  #/qualisys_map/mur620d/pose
+  #/mur620b/UR10_r/global_tcp_pose_mocap
+  #/mur620d/UR10_r/global_tcp_pose_mocap
   /teleop_debug/mur620b_UR10_l/master_wrench_filtered
   /teleop_debug/mur620b_UR10_l/slave_wrench_filtered
   /teleop_debug/mur620b_UR10_r/master_wrench_filtered
   /teleop_debug/mur620b_UR10_r/slave_wrench_filtered
-  /teleop_debug/mur620d_UR10_l/slave_wrench_filtered
-  /teleop_debug/mur620d_UR10_r/slave_wrench_filtered
+  #/teleop_debug/mur620d_UR10_l/slave_wrench_filtered
+  #/teleop_debug/mur620d_UR10_r/slave_wrench_filtered
   /teleop_master_haptic_controller_left/debug/admittance_stats
   /teleop_master_haptic_controller_left/debug/admittance_dynamics
   /teleop_master_haptic_controller_left/debug/dt_stats
   /teleop_master_haptic_controller_left/debug/v_cmd_pre
   /teleop_master_haptic_controller_left/debug/v_cmd_post
-  /teleop_master_haptic_controller_left/debug/passivity_stats
+  #/teleop_master_haptic_controller_left/debug/passivity_stats
   /teleop_master_haptic_controller_right/debug/admittance_stats
   /teleop_master_haptic_controller_right/debug/admittance_dynamics
   /teleop_master_haptic_controller_right/debug/dt_stats
   /teleop_master_haptic_controller_right/debug/v_cmd_pre
   /teleop_master_haptic_controller_right/debug/v_cmd_post
-  /teleop_master_haptic_controller_right/debug/passivity_stats
-  /mur620b/UR10_l/ur_hardware_interface/robot_mode
-  /mur620b/UR10_l/ur_hardware_interface/safety_mode
-  /mur620b/UR10_l/ur_hardware_interface/robot_program_running
-  /mur620b/UR10_r/ur_hardware_interface/robot_mode
-  /mur620b/UR10_r/ur_hardware_interface/safety_mode
-  /mur620b/UR10_r/ur_hardware_interface/robot_program_running
-  /mur620d/UR10_l/ur_hardware_interface/robot_mode
-  /mur620d/UR10_l/ur_hardware_interface/safety_mode
-  /mur620d/UR10_l/ur_hardware_interface/robot_program_running
-  /mur620d/UR10_r/ur_hardware_interface/robot_mode
-  /mur620d/UR10_r/ur_hardware_interface/safety_mode
-  /mur620d/UR10_r/ur_hardware_interface/robot_program_running
+  #/teleop_master_haptic_controller_right/debug/passivity_stats
+  #/mur620b/UR10_l/ur_hardware_interface/robot_mode
+  #/mur620b/UR10_l/ur_hardware_interface/safety_mode
+  #/mur620b/UR10_l/ur_hardware_interface/robot_program_running
+  #/mur620b/UR10_r/ur_hardware_interface/robot_mode
+  #/mur620b/UR10_r/ur_hardware_interface/safety_mode
+  #/mur620b/UR10_r/ur_hardware_interface/robot_program_running
+  #/mur620d/UR10_l/ur_hardware_interface/robot_mode
+  #/mur620d/UR10_l/ur_hardware_interface/safety_mode
+  #/mur620d/UR10_l/ur_hardware_interface/robot_program_running
+  #/mur620d/UR10_r/ur_hardware_interface/robot_mode
+  #/mur620d/UR10_r/ur_hardware_interface/safety_mode
+  #/mur620d/UR10_r/ur_hardware_interface/robot_program_running
   /tf_static
 )
 
